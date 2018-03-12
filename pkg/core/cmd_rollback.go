c9f172d978557341f7b40383a21b67121f56d081
Jay Taylor <outtatime@gmail.com>
2018-03-11 18:54:16 -0700

    WIP.

--- pkg/core/cmd_rollback.go
+++ pkg/core/cmd_rollback.go
@@ -5,6 +5,8 @@ import (
 	"fmt"
 	"net"
 	"time"
+
+	"github.com/jaytaylor/shipbuilder/pkg/domain"
 )
 
 func (server *Server) Rollback(conn net.Conn, applicationName, version string) error {
@@ -51,6 +53,18 @@ func (server *Server) Rollback(conn net.Conn, applicationName, version string) e
 			}
 		}()
 
+		var release domain.Release
+
+		if release, err = deployment.ResolveEarliestReleaseAlias(version); err != nil {
+			return err
+		}
+
+		// Avoid LXC complaints about duplicate fingerprints.
+		if version != release.Version {
+			fmt.Fprintf(logger, "Found original version of %v => %v, will use the original for fingerprint=%v", version, release.Version, release.ImageFingerprint)
+			version = release.Version
+		}
+
 		if err := deployment.extract(version); err != nil {
 			return err
 		}
@@ -62,4 +76,42 @@ func (server *Server) Rollback(conn net.Conn, applicationName, version string) e
 		}
 		return nil
 	})
+}
+
+// ResolveEarliestReleaseAlias finds the root (earliest) version with image
+// fingerprint matching the requested version.
+func (d *Deployment) ResolveEarliestReleaseAlias(version string) (domain.Release, error) {
+	releases, err := d.Server.ReleasesProvider.List(d.Application.Name)
+	if err != nil {
+		return domain.Release{}, err
+	}
+
+	var fingerprint string
+
+	// More recent releases are more likely to be what is requested, so iterate
+	// from newest to oldest (releases are already in order from newest to
+	// oldest).
+	for _, release := range releases {
+		if release.Version == version {
+			if release.ImageFingerprint == "" {
+				return domain.Release{}, errors.New("no fingerprint found for requested version")
+			}
+			fingerprint = release.ImageFingerprint
+			break
+		}
+	}
+
+	if fingerprint == "" {
+		return domain.Release{}, errors.New("requested version not found")
+	}
+
+	// Now iterate from oldest to newest to find the first occurrence of matching
+	// fingerprint.
+	for i := len(releases) - 1; i >= 0; i-- {
+		if releases[i].ImageFingerprint == fingerprint {
+			return releases[i], nil
+		}
+	}
+
+	return domain.Release{}, fmt.Errorf("this should never happen, but somehow no fingerprints matching %q were found", fingerprint)
 }
